e46e6e84f3ddc99b3bdc143b828c817073678fe1
NamYoung Kim <095201a@naver.com>
2022-04-27 18:53:56 +0900

    경로명 변경

--- src/main/java/me/daniel/mychickenshop/MyChickenShopApplication.java
+++ src/main/java/me/daniel/mychickenshop/MyChickenShopApplication.java
@@ -1,4 +1,4 @@
-package com.example.mychickenshop;
+package me.daniel.mychickenshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/me/daniel/mychickenshop/MyChickenShopApplicationTests.java
+++ src/test/java/me/daniel/mychickenshop/MyChickenShopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mychickenshop;
+package me.daniel.mychickenshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
