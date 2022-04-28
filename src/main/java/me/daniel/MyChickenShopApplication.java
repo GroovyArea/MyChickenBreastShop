7c5c5cb85b9ca7221616e9a754d60d53743d02bc
NamYoung Kim <095201a@naver.com>
2022-04-28 10:23:00 +0900

    패키지 수정

--- src/main/java/me/daniel/MyChickenShopApplication.java
+++ src/main/java/me/daniel/MyChickenShopApplication.java
@@ -1,5 +1,6 @@
-package me.daniel.mychickenshop;
+package me.daniel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
