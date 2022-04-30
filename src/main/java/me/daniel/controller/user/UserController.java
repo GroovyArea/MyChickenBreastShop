d3ea5398e2ac1c13abbd038e21b109344d46962f
NamYoung Kim <095201a@naver.com>
2022-04-30 14:13:41 +0900

    insertTest

--- src/main/java/me/daniel/controller/user/UserController.java
+++ src/main/java/me/daniel/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package me.daniel.controller.user;
 
+import lombok.RequiredArgsConstructor;
 import me.daniel.domain.UserVO;
 import me.daniel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-
-
-    @Autowired
-    public UserController(UserService userService){
+/*    @Autowired
+    public UserController(UserService userService) {
         this.userService = userService;
-    }
+    }*/
 
     @GetMapping("/insertTest")
-    public Integer insertTest(){
+    public Integer insertTest() {
 
         userService.insertTest(new UserVO(1, "김남영"));
         return 1;
@@ -28,10 +28,18 @@ public class UserController {
 
 
     @GetMapping("/insertTest2")
-    public Integer insertTest2(){
+    public Integer insertTest2() {
 
         userService.insertTest(new UserVO(2, "김공룡"));
 
         return 2;
+    }
+
+    @GetMapping("/insertTest3")
+    public Integer insertTest3() {
+
+        userService.insertTest(new UserVO(3, "김냐냐"));
+
+        return 3;
     }
 }
