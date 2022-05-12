6a037451c2263ea62876e72a878c9a5e7507be5e
NamYoung Kim <095201a@naver.com>
2022-05-12 14:46:16 +0900

    Body에 담을 Message 클래스 생성

---
+++ src/main/java/me/daniel/responseMessage/Message.java
@@ -0,0 +1,43 @@
+package me.daniel.responseMessage;
+
+/**
+ * 응답 Body에 보낼 메시지 클래스
+ */
+public class Message {
+
+    private Object data;
+    private String message;
+
+    public static class Builder {
+
+        private Object data;
+        private String message;
+
+
+        public Builder(Object data) {
+            this.data = data;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Message build() {
+            return new Message(this);
+        }
+    }
+
+    private Message(Builder builder) {
+        data = builder.data;
+        message = builder.message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
