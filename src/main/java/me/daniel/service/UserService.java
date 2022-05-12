fdcfdaa361223ce2784a6ce25db398d8ac66c787
NamYoung Kim <095201a@naver.com>
2022-05-12 15:18:21 +0900

    암호화 추가

--- src/main/java/me/daniel/service/UserService.java
+++ src/main/java/me/daniel/service/UserService.java
@@ -5,9 +5,12 @@ import me.daniel.domain.DTO.UserModifyDTO;
 import me.daniel.domain.VO.UserVO;
 import me.daniel.exception.UserExistsException;
 import me.daniel.mapper.UserMapper;
+import me.daniel.utility.PasswordEncrypt;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UserService {
@@ -26,10 +29,14 @@ public class UserService {
     }
 
     @Transactional
-    public void addUser(UserDTO joinUser) throws UserExistsException {
+    public void addUser(UserDTO joinUser) throws UserExistsException, NoSuchAlgorithmException {
         if (userMapper.selectUser(joinUser.getUserId()) != null) {
             throw new UserExistsException("이미 사용중인 아이디를 입력 하셨습니다.", joinUser);
         }
+
+        String salt = PasswordEncrypt.getSalt();
+        joinUser.setUserSalt(salt);
+        joinUser.setUserPw(PasswordEncrypt.getSecurePassword(joinUser.getUserPw(), salt));
 
         userMapper.insertUser(modelMapper.map(joinUser, UserVO.class));
     }
