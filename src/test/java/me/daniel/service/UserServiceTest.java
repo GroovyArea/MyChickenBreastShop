a34547b6de9a17bf14dabbe85451a67b80c76391
NamYoung Kim <095201a@naver.com>
2022-05-13 14:04:56 +0900

    로그인 테스트

---
+++ src/test/java/me/daniel/service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package me.daniel.service;
+
+import me.daniel.domain.DTO.UserDTO;
+import me.daniel.exception.LoginAuthFailException;
+import me.daniel.mapper.UserMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UserService userService;
+
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUserDTO(){
+        userDTO= new UserDTO();
+        userDTO.setUserId("aaa333");
+        userDTO.setUserPw("asdf11");
+    }
+
+    @Test
+    @DisplayName("비밀번호 틀렸을 때")
+    void loginAuthTest(){
+        assertThatExceptionOfType(LoginAuthFailException.class)
+                .isThrownBy(() -> userService.loginAuth(userDTO)).withMessage("비밀번호가 일치하지 않습니다.");
+    }
+
+}
