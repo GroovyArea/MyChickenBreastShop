72ad58e596a3f3c01ebd37fe3ae9a4b829dfec5a
NamYoung Kim <095201a@naver.com>
2022-05-17 10:48:09 +0900

    필드 삭제

--- src/main/java/me/daniel/exception/UserExistsException.java
+++ src/main/java/me/daniel/exception/UserExistsException.java
@@ -1,26 +1,13 @@
 package me.daniel.exception;
 
-import me.daniel.domain.DTO.UserDTO;
-import me.daniel.domain.VO.UserVO;
-
 public class UserExistsException extends Exception {
-
-    private UserDTO user;
 
     public UserExistsException() {
 
     }
 
-    public UserExistsException(String message, UserDTO user) {
+    public UserExistsException(String message) {
         super(message);
-        this.user = user;
     }
 
-    public UserDTO getUser() {
-        return user;
-    }
-
-    public void setUser(UserDTO user) {
-        this.user = user;
-    }
 }
