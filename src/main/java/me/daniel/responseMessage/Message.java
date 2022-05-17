890c8aa196e5f832ca8d886e0aeafa20f97d39fc
NamYoung Kim <095201a@naver.com>
2022-05-17 10:44:01 +0900

    필드 추가

--- src/main/java/me/daniel/responseMessage/Message.java
+++ src/main/java/me/daniel/responseMessage/Message.java
@@ -1,5 +1,8 @@
 package me.daniel.responseMessage;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 /**
  * 응답 Body에 보낼 메시지 클래스
  * Builder 패턴 적용
@@ -8,11 +11,15 @@ public class Message {
 
     private Object data;
     private String message;
+    private MediaType mediaType;
+    private HttpStatus httpStatus;
 
     public static class Builder {
 
         private Object data;
         private String message;
+        private MediaType mediaType;
+        private HttpStatus httpStatus;
 
         public Builder(Object data) {
             this.data = data;
@@ -20,6 +27,16 @@ public class Message {
 
         public Builder message(String message) {
             this.message = message;
+            return this;
+        }
+
+        public Builder mediaType(MediaType mediaType) {
+            this.mediaType = mediaType;
+            return this;
+        }
+
+        public Builder httpStatus(HttpStatus httpStatus) {
+            this.httpStatus = httpStatus;
             return this;
         }
 
@@ -31,6 +48,8 @@ public class Message {
     private Message(Builder builder) {
         data = builder.data;
         message = builder.message;
+        mediaType = builder.mediaType;
+        httpStatus = builder.httpStatus;
     }
 
     public Object getData() {
@@ -39,5 +58,13 @@ public class Message {
 
     public String getMessage() {
         return message;
+    }
+
+    public MediaType getMediaType() {
+        return mediaType;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 }
