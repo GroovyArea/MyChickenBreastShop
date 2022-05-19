87b4ddd3874e98ccceee41e701adfc6719489e36
NamYoung Kim <095201a@naver.com>
2022-05-19 23:41:44 +0900

    상수 제거

--- src/main/java/me/daniel/enums/global/ResponseMessage.java
+++ src/main/java/me/daniel/enums/global/ResponseMessage.java
@@ -2,7 +2,6 @@ package me.daniel.enums.global;
 
 public enum ResponseMessage {
 
-    LOGOUT_MESSAGE("Logout succeed"),
     ADD_MESSAGE("Add successful"),
     MODIFY_MESSAGE("Modify successful"),
     DELETE_MESSAGE("Delete successful");
