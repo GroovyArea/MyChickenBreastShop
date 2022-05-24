e38ae1eb056ab9ef9d8400d8540d53d4cab54a1e
NamYoung Kim <095201a@naver.com>
2022-05-24 16:51:08 +0900

    fix : 인증, 인가 분리

--- src/main/java/me/daniel/interceptor/auth/AuthenticateInterceptor.java
+++ src/main/java/me/daniel/interceptor/auth/AuthenticateInterceptor.java
@@ -9,38 +9,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static me.daniel.interceptor.auth.Auth.Role.ADMIN;
-
 /**
- * 권한 처리 인터셉터 <br>
- * 토큰 검증 및 에너테이션 권한 처리를 실행한다.
+ * 인증 인터셉터 <br>
+ * 토큰 검증의 인증 처리를 실행한다.
  *
  * <pre>
  *     <b>History</b>
- *     김남영, 1.0, 2022.05.20 최초 작성
+ *     김남영, 1.0, 2022.05.24 최초 작성
  * </pre>
  *
  * @author 김남영
  * @version 1.0
  */
 @Component
-public class AuthInterceptor implements HandlerInterceptor {
+public class AuthenticateInterceptor implements HandlerInterceptor {
 
-    private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
+    private static final Logger log = LoggerFactory.getLogger(AuthenticateInterceptor.class);
     private static final String BEARER_TOKEN = "Bearer";
 
     private final AuthorizationExtractor authorizationExtractor;
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisTemplate<String, String> redisTemplate;
 
-    public AuthInterceptor(AuthorizationExtractor authorizationExtractor, JwtTokenProvider jwtTokenProvider, RedisTemplate<String, String> redisTemplate) {
+    public AuthenticateInterceptor(AuthorizationExtractor authorizationExtractor, JwtTokenProvider jwtTokenProvider, RedisTemplate<String, String> redisTemplate) {
         this.authorizationExtractor = authorizationExtractor;
         this.jwtTokenProvider = jwtTokenProvider;
         this.redisTemplate = redisTemplate;
@@ -48,26 +44,15 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        if (!(handler instanceof HandlerMethod)) {
-            return true;
-        }
-
-        /* 핸들러메서드 에너테이션 값 추출 */
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
-
-        if (auth == null) {
-            return true;
-        }
-
         /* 토큰 추출 및 검증 */
         String requestToken = authorizationExtractor.extract(request, BEARER_TOKEN);
         jwtTokenProvider.validateToken(requestToken);
 
         /* 토큰 body에 존재하는 아이디와 등급 */
         final String tokenUserId = jwtTokenProvider.getUserId(requestToken);
-        final String tokenUserRole = jwtTokenProvider.getUserGrade(requestToken);
+
+        /* request에 토큰 유저 권한 추가 */
+        request.setAttribute("tokenUserRole", jwtTokenProvider.getUserGrade(requestToken));
 
         /* Redis DB에 저장된 토큰 추출 */
         final ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
@@ -83,13 +68,6 @@ public class AuthInterceptor implements HandlerInterceptor {
             throw new TokenMismatchException(AuthMessages.INVALID_TOKEN.getMessage());
         }
 
-        /* 에너테이션 값 => 관리자일 경우 */
-        if (auth.role() == ADMIN) {
-            /* 로그인 유저 권한이 관리자가 아닐 경우 */
-            if (!tokenUserRole.equals(ADMIN.toString())) {
-                throw new AuthenticationException(AuthMessages.NOT_ADMIN_AUTH.getMessage());
-            }
-        }
         return true;
     }
 }
---
+++ src/main/java/me/daniel/interceptor/auth/AuthorizeInterceptor.java
@@ -0,0 +1,61 @@
+package me.daniel.interceptor.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static me.daniel.interceptor.auth.Auth.Role.ADMIN;
+
+/**
+ * 권한 처리 인터셉터 <br>
+ * 에너테이션 권한 처리를 실행한다.
+ *
+ * <pre>
+ *     <b>History</b>
+ *     김남영, 1.0, 2022.05.20 최초 작성
+ *     김남영, 1.1, 2022.05.24 인증, 인가 분리
+ * </pre>
+ *
+ * @author 김남영
+ * @version 1.1
+ */
+@Component
+public class AuthorizeInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthorizeInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        /* 핸들러메서드 에너테이션 값 추출 */
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
+
+        /* 권한이 필요 없는 접근 */
+        if (auth == null) {
+            return true;
+        }
+
+        /* 에너테이션 값 => 관리자일 경우 */
+        if (auth.role() == ADMIN) {
+            /* 로그인 유저 권한이 관리자가 아닐 경우 */
+            if (!request.getAttribute("tokenUserRole").toString().equals(ADMIN.toString())) {
+                throw new AuthenticationException(AuthMessages.NOT_ADMIN_AUTH.getMessage());
+            }
+        }
+
+        return true;
+    }
+
+
+}
