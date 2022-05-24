ac2c22c413498aa7f8ea135dede4dd434f34249b
NamYoung Kim <095201a@naver.com>
2022-05-24 15:47:47 +0900

    add : CookieInterceptor 추가

--- src/main/java/me/daniel/config/WebConfig.java
+++ src/main/java/me/daniel/config/WebConfig.java
@@ -1,6 +1,7 @@
 package me.daniel.config;
 
 import me.daniel.interceptor.auth.AuthInterceptor;
+import me.daniel.interceptor.cookie.CookieInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +14,20 @@ public class WebConfig implements WebMvcConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
     private final AuthInterceptor authInterceptor;
+    private final CookieInterceptor cookieInterceptor;
 
-    public WebConfig(AuthInterceptor authInterceptor) {
+    public WebConfig(AuthInterceptor authInterceptor, CookieInterceptor cookieInterceptor) {
         this.authInterceptor = authInterceptor;
+        this.cookieInterceptor = cookieInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**");
+        registry.addInterceptor(cookieInterceptor)
+                .addPathPatterns("/api/carts/**");
+
     }
 
 
---
+++ src/main/java/me/daniel/interceptor/cookie/CookieInterceptor.java
@@ -0,0 +1,37 @@
+package me.daniel.interceptor.cookie;
+
+import me.daniel.exceptions.EmptyCookiesException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 쿠키 인터셉터
+ * 쿠키가 들어 있는지 검사한다.
+ *
+ * <pre>
+ *     <b>History</b>
+ *     김남영, 1.0, 2022.05.24 최초 작성
+ * </pre>
+ *
+ * @author 김남영
+ * @version 1.0
+ */
+
+@Component
+public class CookieInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        if (request.getCookies() == null) {
+            throw new EmptyCookiesException("쿠키가 없습니다.");
+        }
+        return true;
+    }
+}
