f931abc0936a66f6d52d193de767a94b70977f1f
NamYoung Kim <095201a@naver.com>
2022-05-26 15:32:26 +0900

    fix : 주석 및 경로 수정

--- src/main/java/me/daniel/controller/user/LoginController.java
+++ src/main/java/me/daniel/controller/user/LoginController.java
@@ -1,10 +1,8 @@
 package me.daniel.controller.user;
 
 import me.daniel.domain.DTO.UserLoginDTO;
-import me.daniel.enums.users.ExceptionMessages;
 import me.daniel.exceptions.LoginFailException;
-import me.daniel.exceptions.UserExistsException;
-import me.daniel.exceptions.WithDrawalUserException;
+import me.daniel.exceptions.WithDrawUserException;
 import me.daniel.exceptions.WrongPasswordException;
 import me.daniel.responseMessage.Message;
 import me.daniel.service.UserService;
@@ -12,7 +10,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -23,7 +24,7 @@ import java.security.NoSuchAlgorithmException;
  * @author 김남영
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class LoginController {
 
     private static final String LOGIN_MESSAGE = "Login succeed";
@@ -42,8 +43,8 @@ public class LoginController {
      * @param userLoginDTO 로그인 DTO
      * @return Message 응답 정보 객체
      */
-    @PostMapping("/user/login")
-    public Message loginAction(@ModelAttribute UserLoginDTO userLoginDTO) throws LoginFailException, NoSuchAlgorithmException, WithDrawalUserException, WrongPasswordException {
+    @PostMapping("/login")
+    public Message loginAction(@ModelAttribute UserLoginDTO userLoginDTO) throws LoginFailException, NoSuchAlgorithmException, WithDrawUserException, WrongPasswordException {
         userService.loginAuth(userLoginDTO);
         String jwtToken = userService.createToken(userLoginDTO);
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
@@ -56,29 +57,4 @@ public class LoginController {
                 .httpStatus(HttpStatus.OK)
                 .build();
     }
-
-    @ExceptionHandler(value = LoginFailException.class)
-    public Message loginFailHandler() {
-        return new Message
-                .Builder(ExceptionMessages.LOGIN_FAIL_MESSAGE.getValue())
-                .httpStatus(HttpStatus.NO_CONTENT)
-                .build();
-    }
-
-    @ExceptionHandler(value = WithDrawalUserException.class)
-    public Message withDrawUserHandler() {
-        return new Message
-                .Builder(ExceptionMessages.WITHDRAWAL_USER_MESSAGE.getValue())
-                .httpStatus(HttpStatus.NO_CONTENT)
-                .build();
-    }
-
-    @ExceptionHandler(value = UserExistsException.class)
-    public Message userExistsHandler() {
-        return new Message
-                .Builder(ExceptionMessages.USER_EXISTS_MESSAGE.getValue())
-                .httpStatus(HttpStatus.NO_CONTENT)
-                .build();
-    }
-
 }
